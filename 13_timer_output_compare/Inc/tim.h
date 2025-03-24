3dcb5f7c0fbf4f15358970c2e7d09ee279a3de1b
Arnav Verma <147163265+arnavV456@users.noreply.github.com>
2025-03-24 09:38:38 +0530

    timer output compare driver

---
+++ 13_timer_output_compare/Inc/tim.h
@@ -0,0 +1,17 @@
+/*
+ * tim.h
+ *
+ *  Created on: Mar 23, 2025
+ *      Author: verma
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+
+void timer2_1hz_init(void);
+void timer2_pa5_output_compare(void);
+
+
+#define SR_UIF		(1U<<0)
+
+#endif /* TIM_H_ */
